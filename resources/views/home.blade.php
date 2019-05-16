aa95422a16d8a8c6c1177fc13a315056ca28574a
Abner <abneralves10@hotmail.com>
2019-05-16 14:33:28 -0300

    Criação da home e extensão do mainTemplate

---
+++ resources/views/home.blade.php
@@ -0,0 +1,282 @@
+@extends('templates.mainTemplate')
+
+@section('content')
+    <div class="slider-area">
+        <!-- Slider -->
+        <div class="block-slider block-slider4">
+            <ul class="trotus" id="bxslider-home4">
+                <li>
+                    <img src="{{asset('img/bolsa.png')}}" alt="Slide">
+                    <div class="caption-group">
+                        <h2 class="caption title">
+                            Square bag Calfskin razza print chain
+                        </h2>
+                        <h4 class="caption subtitle">Square bag with plaque/magnet fastening and gold chain. Leather
+                            surface treated with rayeffect resin.Gold-plated brass logo ont front</h4>
+                    </div>
+                </li>
+                <li><img src="{{asset('img/bolsa1.png')}}" alt="Slide">
+                    <div class="caption-group">
+                        <h2 class="caption title">
+                            Square bag Calfskin razza print chain
+                        </h2>
+                        <h4 class="caption subtitle">Square bag with plaque/magnet fastening and gold chain. Leather
+                            surface treated with rayeffect resin.Gold-plated brass logo ont front</h4>
+                    </div>
+                </li>
+                <li><img src="{{asset('img/bolsa2.png')}}" alt="Slide">
+                    <div class="caption-group">
+                        <h2 class="caption title">
+                            Square bag Calfskin razza print chain
+                        </h2>
+                        <h4 class="caption subtitle">Square bag with plaque/magnet fastening and gold chain. Leather
+                            surface treated with rayeffect resin.Gold-plated brass logo ont front</h4>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="maincontent-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="latest-product">
+                        <h2 class="section-title">Quem viu, viu também</h2>
+                        <div class="product-carousel">
+                            <div class="single-product">
+                                <h2>Fourlard Cruciani C <br>100 siik 45x45</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <h2>Fow-leat CLover</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <h2>Fow-leat CLover</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <h2>Fow-leat CLover</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="latest-product">
+                        <h2 class="section-title">Quem comprou, comprou também</h2>
+                        <div class="product-carousel">
+                            <div class="single-product">
+                                <h2>Fourlard Cruciani C <br>100 siik 45x45</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <h2>Fow-leat CLover</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <h2>Fow-leat CLover</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <h2>Fow-leat CLover</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="latest-product">
+                        <h2 class="section-title">Promoção semanal</h2>
+                        <div class="product-carousel">
+                            <div class="single-product">
+                                <h2>Fourlard Cruciani C <br>100 siik 45x45</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <h2>Fow-leat CLover</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <h2>Fow-leat CLover</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <h2>Fow-leat CLover</h2>
+                                <div class="product-carousel-price">
+                                    <ins>$899.00</ins>
+                                    <del>$999.00</del>
+                                </div>
+                                <div class="product-f-image">
+                                    <img src="{{asset('img/quarter.png')}}" alt="">
+                                    <div class="product-hover">
+                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="promo-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo1">
+                        <i class="fa fa-cut"></i>
+                        <p>Produzido na Itália</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo2">
+                        <i class="fa fa-truck"></i>
+                        <p>Entrega Rápida</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo3">
+                        <i class="fa fa-lock"></i>
+                        <p>Pagamento seguro</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-promo promo4">
+                        <i class="fa fa-anchor"></i>
+                        <p>Garantia</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <!-- Latest jQuery form server -->
+    <script src="https://code.jquery.com/jquery.min.js"></script>
+
+    <!-- Bootstrap JS form CDN -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+    <!-- jQuery sticky menu -->
+    <script src="{{asset('js/jquery.sticky.js')}}"></script>
+
+    <!-- Main Script -->
+    <script src="{{asset('js/main.js')}}"></script>
+
+    <!-- Slider -->
+    <script type="text/javascript" src="{{asset('js/bxslider.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/script.slider.js')}}"></script>
+@endsection
